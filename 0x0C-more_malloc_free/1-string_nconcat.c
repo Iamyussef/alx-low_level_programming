b0f646e5c23b3df64764e72fd1ede037585c69d1
Iamyussef <youssefidomar123@gmail.com>
2023-09-07 19:08:08 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		}
 		else
 		{
-			m = malloc(sizeof(char) * (n + s1_size));
+			m = malloc(sizeof(char) * (n + s1_size) + 1);
 		}
 		if (m == NULL)
 		{
