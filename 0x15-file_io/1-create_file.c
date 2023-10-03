14d673e9ecd9bee2ca838da06e6aa93821a90f50
Iamyussef <youssefidomar123@gmail.com>
2023-10-02 18:42:46 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,15 +14,15 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes = 0, size = strlen(text_content);
+	ssize_t bytes = 0, len = _strlen(text_content);
 
-		if (filename == NULL)
-			return (-1);
+	if (!filename)
+		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
-		if (fd == -1)
-			return (-1);
-		if (size)
-			bytes = write(fd, text_content, size);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
 	close(fd);
-	return (bytes == size ? 1 : -1);
+	return (bytes == len ? 1 : -1);
 }
