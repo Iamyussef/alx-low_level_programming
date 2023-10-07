b0ed55cf55b934f2ed3edae872a23f2ac8c22044
Iamyussef <youssefidomar123@gmail.com>
2023-10-06 18:58:25 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,45 +1,25 @@
-#include <stdio.h>
 #include "main.h"
-#include <fcntl.h>
 #include <unistd.h>
-
-#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
-
+#include <fcntl.h>
+#include <string.h>
 /**
- * main - program
- * @ac: number of arguments passed
- * @av: argument passed
- *
+ * append_text_to_file - appends text to a file
+ * @filename: name of file to create
+ * @text_content: text to write
  * Return: 1 on success 0 on failure
  */
-
-int main(int ac, char **av)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int from_fd = 0, to_fd = 0;
-	ssize_t b;
-	char buf[1024];
+	int fd;
+	ssize_t bytes = 0, size = strlen(text_content);
 
-	if (ac != 3)
-		dprintf(STDERR_FILENO,"Usage: cp file_from file_to\n"), exit(97);
-	from_fd = open(av[1], O_RDONLY);
-	if (from_fd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-	if (to_fd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-
-	while ((b = read(from_fd, buf,1024)) > 0)
-		if (write(to_fd, buf, b) != b)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	if (b == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-
-	from_fd = close(from_fd);
-	to_fd = close(to_fd);
-	if (from_fd)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd), exit(100);
-	if (to_fd)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd), exit(100);
-
-	return (EXIT_SUCCESS);
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (size)
+		bytes = write(fd, text_content, size);
+	close(fd);
+	return (bytes == size ? 1 : -1);
 }
